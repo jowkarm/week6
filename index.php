ecb6b2ac3c7892d43ac71d00164dcaf86137001d
Mehdi Jokar <jokar.mehdi2@gmail.com>
2023-05-09 13:55:48 -0400

    Edit info.html

--- index.php
+++ index.php
@@ -23,7 +23,37 @@ require_once ('vendor/autoload.php');
 $f3 = Base::instance();
 
 // Define a default route
-$f3->route('GET /', function () {
+$f3->route('GET /', function ($f3) {
+
+    // Add data to F3 "hive"
+    $f3->set('color', 'green');
+    $f3->set('food', 'pho');
+    $f3->set('title', 'Welcome to week 6!');
+    $f3->set('temp', 67);
+
+    // Add a radius variable to your controller
+    // Template: Print the circumference of a circle
+    // C = 2 * PI * radius
+    // PHP has a pi() function
+    // Round to 3 decimal places
+    $f3->set('radius', 14);
+
+    $fruits = array('banana', 'apple', 'kiwi');
+    $f3->set('fruits', $fruits);
+
+    $vegetables  = array('Broccoli', 'Spinach', 'Carrots', 'Bell peppers', 'Tomatoes');
+    $f3->set('vegetables', $vegetables );
+
+
+    // Define an associative array of cupcake flavors
+    $cupcakes = array(
+        "chocolate" => "Chocolate Cupcake",
+        "vanilla" => "Vanilla Cupcake",
+        "red_velvet" => "Red Velvet Cupcake",
+        "lemon" => "Lemon Cupcake",
+        "strawberry" => "Strawberry Cupcake"
+    );
+    $f3->set('cupcakes', $cupcakes );
 
     // Display a view page
     $view = new Template();
